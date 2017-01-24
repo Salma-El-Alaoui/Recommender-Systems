72c6fdf377c7b604eb9efeba6a7196fd3185fdd3
LIU Zhengying <zhengying.liu@polytechnique.edu>
2017-01-24 22:43:05 +0100

    ALS_WR - one-thread version
    about 55 sec per iteration for the small dataset MovieLens “S”

---
+++ src/ALS_WR/als_wr.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 11:35:18 2017
+
+@author: Evariste
+"""
+
+import numpy as np
+import pandas as pd
+import os
+import sys
+import time
+
+# Add parent directory to python path
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+from data_fetching.data_fetching import getDataframe
+
+def ALS_WR(df_train):
+    print("Beginning...")
+    
+    # Hyperparameters
+    # r : number of latent features
+    # lmda : penalization coefficient
+    r = 10
+    lmda = 0.065
+    
+    n_users = df_train.user_id.unique().size
+    n_items = df_train.item_id.unique().size
+    
+    X = np.random.rand(n_users,r)
+    Y = np.random.rand(n_items,r)
+    
+    # Input
+    #   Y: feature matrix of movies
+    #   u: a user_id
+    #   Du: pd.DataFrame corresponding to u
+    # Output
+    #   the feature vector x_u for user u
+    def findXu(Y,Du):
+        nu = Du.shape[0]
+        Au = nu * lmda * np.eye(r)
+        Vu = np.zeros(r)
+        for index, row in Du.iterrows():
+            yi = Y[row.item_id-1]
+            rui = row.rating
+            Au += yi[:,None] * yi[None,:]
+            Vu += rui * yi
+        return np.linalg.solve(Au,Vu)
+        
+    # Input
+    #   X: feature matrix of movies
+    #   i: a user_id
+    #   Du: pd.DataFrame corresponding to u
+    # Output
+    #   the feature vector x_u for user u
+    def findYi(X,Di):
+        nu = Di.shape[0]
+        Ai = nu * lmda * np.eye(r)
+        Vi = np.zeros(r)
+        for index, row in Du.iterrows():
+            xu = X[row.user_id-1]
+            rui = row.rating
+            Ai += xu[:,None] * xu[None,:]
+            Vi += rui * xu
+        return np.linalg.solve(Ai,Vi)
+    
+    print("Begin groupby")
+    grouped_by_userid = df_train.groupby(['user_id'])
+    print("2e groupby")
+    grouped_by_itemid = df_train.groupby(['item_id'])
+    print("Begin iteration.")
+    
+    for _ in range(10):
+        t1 = time.time()
+        for user in range(n_users):
+            uid = user + 1
+            Du = grouped_by_userid.get_group(uid)
+            X[user] = findXu(Y,Du)
+        for item in range(n_items):
+            iid = item + 1
+            Di = grouped_by_itemid.get_group(iid)
+            Y[item] = findYi(X,Di)
+        t2 = time.time()
+        print(str(_)+"-th iteration, time: "+str(t2-t1))
+    return X, Y
+    
+#df = getDataframe()
+
+
+
+
+
+
+#if __name__ == "__main__":
+#    print("haha")
